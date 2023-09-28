51e6b6ec0acb95b28422fe037a5df740126c27f5
Edward L. <59051661+Justedlev@users.noreply.github.com>
2023-09-28 18:56:49 +0300

    -

--- src/main/java/com/justedlev/hub/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/justedlev/hub/configuration/security/SecurityConfiguration.java
@@ -3,15 +3,11 @@ package com.justedlev.hub.configuration.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -50,14 +46,13 @@ public class SecurityConfiguration {
                 )
                 .oauth2ResourceServer(oAuth -> oAuth
                         .jwt(jwtSpec -> jwtSpec
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                                .jwtAuthenticationConverter(reactiveJwtAuthenticationConverterAdapter())
                         )
                 )
                 .build();
     }
 
-    @Bean
-    public Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+    private ReactiveJwtAuthenticationConverterAdapter reactiveJwtAuthenticationConverterAdapter() {
         return new ReactiveJwtAuthenticationConverterAdapter(keycloakJwtAuthenticationConverter);
     }
 }
